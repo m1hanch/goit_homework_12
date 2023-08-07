f17425d01ed18a8fc09ecaeef0c670b3c9d82a87
m1hanch <misha5264@gmail.com>
2023-08-07 08:45:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import calendar
+import csv
 from collections import UserDict
 from datetime import datetime
 import pickle
@@ -74,7 +75,7 @@ class Record:
             phone = []
         self.name = name
         self.phones = list(phone)
-        self.bday = birthday
+        self.birthday = birthday
 
     #функція додавання телефону
     def add_phone(self, phone: Phone):
@@ -94,8 +95,8 @@ class Record:
 
     #функіця, яка рахує кількість днів до наступного дня народження даного контакту
     def days_to_birthday(self) -> int:
-        if self.bday is not None:
-            bday = self.bday.birthday[:-4] + str(datetime.now().year)
+        if self.birthday is not None:
+            bday = self.birthday.birthday[:-4] + str(datetime.now().year)
             bday = datetime.strptime(bday, '%d.%m.%Y')
             now = datetime.now()
             result = bday.toordinal() - now.toordinal() #обчислення різниці днів між двома датами у поточному році
@@ -132,18 +133,46 @@ class AddressBook(UserDict):
         for group in grouped:
             yield group
 
+    """"функція завантажує до вказаного файлу дані, 
+        які знаходяться в об'єкті AddressBook на момент її виклику"""
     def load_to_disk(self, filename: str):
         with open(filename, 'ab') as fh:
             pickle.dump(self.data,fh)
+
+    """"функція, яка зі вказаного файлу завантажує дані до об'єкту AddressBook"""
     def load_from_disk(self, filename: str):
             with open(filename, 'rb') as fh:
                 self.data = pickle.load(fh)
 
+    """"функція, яка повертає список записів у об'єкті AddressBook,
+        номери яких, співпадають із поданим, як параметр, фрагментом номеру"""
     def search_by_number(self, number: str) -> list[Record]:
         return [record for record in self.data.values() for phone in record.phones if number in phone.phone]
 
+    """"функція, яка повертає список записів у об'єкті AddressBook,
+        імена яких, співпадають із поданим, як параметр, фрагментом імені"""
     def search_by_name(self, name: str) -> list[Record]:
         return [self.data[n] for n in self.data.keys() if name.upper() in n.upper()]
+
+    """"функція, яка створює файл csv і записує туди, у відповідній формі,
+        усі дані, які знаходяться у об'єкті AddressBook"""
+    def create_csv_table(self, name_of_table: str):
+        with open(name_of_table,'w', newline='') as fh:
+            """"заголовки стовпців таблиці"""
+            field_names = ['name', 'birthday', 'phones']
+            csv.writer(fh).writerow(field_names)
+            """"список словників, кожен з яких має ключі 'name', 'birthday' та 'phones'"""
+            formatted_list = []
+            for value in self.data.values():
+                formatted_list.append({'name':value.name.name, 'birthday':value.birthday, 'phones': [value.phones[i].phone for i in range(len(value.phones))]})
+            writer = csv.DictWriter(fh, fieldnames=field_names)
+            writer.writerows(formatted_list)
+
+    """"функція зчитує та повертає у вигляді списку словників дані з файлу з таблицею csv"""
+    def read_from_csv_table(self, name_of_table: str):
+        with open(name_of_table, newline='') as fh:
+            reader = csv.DictReader(fh)
+            return [row for row in reader]
 
 
 
@@ -161,6 +190,7 @@ if __name__ == "__main__":
     phone = Phone('1234567893')
     rec3 = Record(name, phone)
     rec4 = Record(Name('Billr'), Phone('1234567894'))
+    rec5 = Record(Name('Billt'),Phone('1234567895'),Phone('1234567896'),Phone('1234567896'), birthday='12.08.1998')
     ab = AddressBook()
     # ab.load_from_disk('disk.bin')
     ab.add_record(rec)
@@ -168,7 +198,8 @@ if __name__ == "__main__":
     ab.add_record(rec2)
     ab.add_record(rec3)
     ab.add_record(rec4)
-    ab.load_to_disk('disk.bin') #зразок виклику функції та назви файлу
+    ab.add_record(rec5)
+    #ab.load_to_disk('disk.bin') #зразок виклику функції та назви файлу
 
     #приклад пошуку контакту за фрагментом номеру телефону
     print([ab.search_by_number('34')[i].name.name for i in range(len(ab.search_by_number('34')))])
@@ -182,3 +213,5 @@ if __name__ == "__main__":
     assert ab['Bill'].phones[0].phone == '1234567890'
     print('All Ok)')
     print(ab.list_contacts())
+    ab.create_csv_table('contacts_table.csv')
+    print(ab.read_from_csv_table('contacts_table.csv'))
